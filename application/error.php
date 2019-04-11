be18eacae8f4266877b0ff4d7afef7cb0d76d3ec
djkirkma <djkirkma@gmail.com>
2019-04-11 12:21:11 -0400

    Error class

--- application/error.php
+++ application/error.php
@@ -1,4 +1,10 @@
 <?php
-echo "An error has occured"
 
-?>
+require_once 'classes/show_error.class.php';
+
+//retrieve the error message from a querystring variable
+$message = $_GET['eMsg'];
+
+//create an Error object and then display the error message
+$error = new ShowError();
+$error->display($message);
